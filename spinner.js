1c3a6d8660518773ba679e2d71eda30e2cd1d4da
BigBadAI <flashmefusion@hotmail.ca>
2021-01-12 16:22:35 +0000

    Fun little function to spin a line in a full cirlce using setTimeout function

---
+++ spinner.js
@@ -0,0 +1,40 @@
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+
+}, 100);
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+
+}, 200);
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+
+}, 300);
+setTimeout(() => {
+  process.stdout.write('\r\\  ');
+
+}, 400);
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+
+}, 500);
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+
+}, 600);
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+
+}, 700);
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+
+}, 800);
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+
+}, 900);
+setTimeout(() => {
+  console.log();
+
+}, 1000);
