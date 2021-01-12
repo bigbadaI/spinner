1c209092e0f509a2dad613e55b31247f1390a79a
BigBadAI <flashmefusion@hotmail.ca>
2021-01-12 20:27:34 +0000

    Updated to use an array and a for of loop instead of the if statements making the code much smaller and simpler

--- spinner2.js
+++ spinner2.js
@@ -1,57 +1,9 @@
-for (let i = 0; i <= 10; i++) {
+let spinnerArray = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', "\n"];
+let i = 1;
+for (const char of spinnerArray) {
   setTimeout(() => {
-    if (i === 1 || i === 5 || i === 9)
-      process.stdout.write('\r|   ');
-    if (i === 2 || i === 6)
-      process.stdout.write('\r/   ');
-    if (i === 3 || i === 7)
-      process.stdout.write('\r-   ');
-    if (i === 4 || i === 8)
-      process.stdout.write('\r\\   ');
-    if (i === 10)
-      console.log();
+    process.stdout.write(char);
   }, (i * 100));
+  i++;
+  // console.log();
 }
-
-
-
-// setTimeout(() => {
-//   process.stdout.write('\r|   ');
-
-// }, 100);
-// setTimeout(() => {
-//   process.stdout.write('\r/   ');
-
-// }, 200);
-// setTimeout(() => {
-//   process.stdout.write('\r-   ');
-
-// }, 300);
-// setTimeout(() => {
-//   process.stdout.write('\r\\  ');
-
-// }, 400);
-// setTimeout(() => {
-//   process.stdout.write('\r|   ');
-
-// }, 500);
-// setTimeout(() => {
-//   process.stdout.write('\r/   ');
-
-// }, 600);
-// setTimeout(() => {
-//   process.stdout.write('\r-   ');
-
-// }, 700);
-// setTimeout(() => {
-//   process.stdout.write('\r\\   ');
-
-// }, 800);
-// setTimeout(() => {
-//   process.stdout.write('\r|   ');
-
-// }, 900);
-// setTimeout(() => {
-//   console.log();
-
-// }, 1000);
